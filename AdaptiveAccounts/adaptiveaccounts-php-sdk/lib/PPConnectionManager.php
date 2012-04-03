ef598d5aefe3476faf3a213e3364ea0ad325e668
ganeshx <chipgeri@gmail.com>
2012-04-03 20:10:43 +0530

    added CA certificate to SDK to make it work on wamp

--- AdaptiveAccounts/adaptiveaccounts-php-sdk/lib/PPConnectionManager.php
+++ AdaptiveAccounts/adaptiveaccounts-php-sdk/lib/PPConnectionManager.php
@@ -28,10 +28,10 @@ class PPConnectionManager
 		$connection = new PPHttpConnection();
 
 		$configMgr = PPConfigManager::getInstance();
-		if( ($configMgr->get("http.ConnectionTimeOut")) ) {
+		if($configMgr->get("http.ConnectionTimeOut") ) {
 			$connection->setHttpTimeout( $configMgr->get("http.ConnectionTimeOut") );
 		}
-		if( $configMgr->get("http.TrustAllConnection") === '1' ) {
+		if( $configMgr->get("http.TrustAllConnection") ) {
 			$connection->setHttpTrustAllConnection( $configMgr->get("http.TrustAllConnection") );
 		}
 		if( $configMgr->get("http.Proxy") ) {
