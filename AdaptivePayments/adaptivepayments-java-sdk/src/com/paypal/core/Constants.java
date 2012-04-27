e6e73ea13e89dfac09138e471ae2bb6e24bfbd76
tkanta <tarininegi@gmail.com>
2012-04-27 14:56:08 +0530

    Fix for incorrect Permissions header (X-PP-AUTHORIZATION)

--- AdaptivePayments/adaptivepayments-java-sdk/src/com/paypal/core/Constants.java
+++ AdaptivePayments/adaptivepayments-java-sdk/src/com/paypal/core/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
 	public static final String SANDBOX_EMAIL_ADDRESS = "Platform.sdk.seller@gmail.com";
 	public static final String SOAP = "SOAP";
 	public static final String SDK_NAME = "sdk-adaptivepayments-java";
-	public static final String SDK_VERSION = "0.6.86";
+	public static final String SDK_VERSION = "0.7.88";
 }
